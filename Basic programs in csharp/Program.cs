260a000b6019e6082663482614bd392415d43bfe
vishnu244 <vishnunali244@gmail.com>
2022-04-21 11:22:31 +0530

    leapyear program

--- Basic programs in csharp/Program.cs
+++ Basic programs in csharp/Program.cs
@@ -7,13 +7,17 @@ Console.WriteLine("Basic programming");
 while (true)
 {
 
-    Console.WriteLine("Choose the Program to be executed :\n1)flipcoinsimulator\n");
+    Console.WriteLine("Choose the Program to be executed :\n1)flipcoinsimulator\n2)leapyear");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             Flipcoin flipCoin = new();
             flipCoin.FlipCoin();
+            break;
+        case 2:
+            Leapyear leapyear = new();
+            leapyear.LeapYear();
             break;
         default: 
             break;
---
+++ Basic programs in csharp/leapyear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_programs_in_csharp
+{
+    public class Leapyear
+    {
+        public void LeapYear()
+        {
+            Console.WriteLine("Welcome to Leap Year Computation!");
+
+            Console.WriteLine("Enter the Year : x");
+            var x = Convert.ToDouble(Console.ReadLine());
+
+            if (((x % 4 == 0) && (x % 100 != 0)) || (x % 400 == 0))
+                Console.WriteLine("{0} is a Leap Year ", x);
+            else
+                Console.WriteLine("{0} is not a Leap Year ", x);
+        }
+    }
+}
