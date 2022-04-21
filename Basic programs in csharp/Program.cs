398c167178161786eef7577c7e4a6469597bced7
vishnu244 <vishnunali244@gmail.com>
2022-04-21 11:07:33 +0530

    Flipcoin program

--- Basic programs in csharp/Program.cs
+++ Basic programs in csharp/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Basic_programs_in_csharp;
+
 Console.WriteLine("Basic programming");
+
+
+while (true)
+{
+
+    Console.WriteLine("Choose the Program to be executed :\n1)flipcoinsimulator\n2)Leapyear");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Flipcoin flipCoin = new();
+            flipCoin.FlipCoin();
+            break;
+        default: 
+            break;
+
+    }
+
+
+}
